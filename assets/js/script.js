9e76234c910d5bd5375863f13b9e67116fd363d9
bdstpierre <barry@stpierre.com>
2021-05-04 23:31:17 -0400

    Added results for today's weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,46 +13,51 @@ function getParams() {
   searchApi(query, format);
 }
 
-function printResults(resultObj) {
+function printTodayResults(resultObj) {
   console.log(resultObj);
 
   // set up `<div>` to hold result content
   var resultCard = document.createElement('div');
   resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
 
-  var resultBody = document.createElement('div');
-  resultBody.classList.add('card-body');
+  
+  var resultHeader = document.createElement('div');
+  resultHeader.classList.add('card-header', 'bg-light', 'text-dark', 'p-3');
+
+  var date = moment.unix(resultObj.dt + resultObj.timezone).format(" (M/D/YYYY) ");
+  var mediaContent = document.createElement('div');
+  mediaContent.classList.add('media');
+  mediaBody = document.createElement('div');
+  mediaBody.classList.add('media-body');
+  var titleHeader = document.createElement('h2');
+  titleHeader.textContent = resultObj.name + date;
+  mediaBody.append(titleHeader);
+  
+  var titleIcon = document.createElement('img');
+  titleIcon.classList.add('float-right', 'img-thumbnail', 'mr-3');
+  titleIcon.src = "http://openweathermap.org/img/wn/"+resultObj.weather[0].icon+"@2x.png";
+  mediaContent.append(mediaBody, titleIcon);
+  resultHeader.append(mediaContent);
+
+  resultCard.append(resultHeader);
+
+  var resultBody = document.createElement('ul');
+  resultBody.classList.add('list-group','list-group-flush', 'list-unstyled');
+  var tempList = document.createElement('li');
+  tempList.classList.add('list-group-item)', 'p-1');
+  tempList.textContent = "Temp: " + resultObj.main.temp + " °F"
+  var windList = document.createElement('li');
+  windList.classList.add('list-group-flush', 'p-1');
+  windList.textContent = "Wind: " + resultObj.wind.speed + " MPH";
+  var humidList = document.createElement('li');
+  humidList.classList.add('list-group-flush', 'p-1');
+  humidList.textContent = "Humidity: " + resultObj.main.humidity + " %";
+  var uvList = document.createElement('li');
+  uvList.classList.add('list-group-flush', 'p-1');
+  uvList.textContent = "UV Index: " + 6;
+  resultBody.append(tempList, windList, humidList, uvList);
+  
   resultCard.append(resultBody);
-
-  var titleEl = document.createElement('h3');
-  titleEl.textContent = resultObj.title;
-
-  var bodyContentEl = document.createElement('p');
-  bodyContentEl.innerHTML =
-    '<strong>Date:</strong> ' + resultObj.date + '<br/>';
-
-  if (resultObj.subject) {
-    bodyContentEl.innerHTML +=
-      '<strong>Subjects:</strong> ' + resultObj.subject.join(', ') + '<br/>';
-  } else {
-    bodyContentEl.innerHTML +=
-      '<strong>Subjects:</strong> No subject for this entry.';
-  }
-
-  if (resultObj.description) {
-    bodyContentEl.innerHTML +=
-      '<strong>Description:</strong> ' + resultObj.description[0];
-  } else {
-    bodyContentEl.innerHTML +=
-      '<strong>Description:</strong>  No description for this entry.';
-  }
-
-  var linkButtonEl = document.createElement('a');
-  linkButtonEl.textContent = 'Read More';
-  linkButtonEl.setAttribute('href', resultObj.url);
-  linkButtonEl.classList.add('btn', 'btn-dark');
-
-  resultBody.append(titleEl, bodyContentEl, linkButtonEl);
 
   resultContentEl.append(resultCard);
 }
@@ -61,11 +66,13 @@ function searchApi(query) {
   // API Documentation:
   // api.openweathermap.org/data/2.5/weather?q={city name}&appid=ae645017a7275733894562a7a4d6f737
   // api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt=5&appid=ae645017a7275733894562a7a4d6f737
-  var locQueryUrl = 'api.openweathermap.org/data/2.5/weather';
-  var locDailyQueryUrl = 'pi.openweathermap.org/data/2.5/forecast/daily';
-  var apiKey = '&appid=ae645017a7275733894562a7a4d6f737';
+  var locQueryUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  var locDailyQueryUrl = 'https://api.openweathermap.org/data/2.5/forecast/daily';
+  var apiKey = '&units=imperial&appid=ae645017a7275733894562a7a4d6f737';
 
-  locQueryUrl = locQueryUrl + '&q=' + query + apiKey;
+  locQueryUrl = locQueryUrl + '?q=' + query + apiKey;
+
+  console.log(locQueryUrl);
 
   fetch(locQueryUrl)
     .then(function (response) {
@@ -77,19 +84,18 @@ function searchApi(query) {
     })
     .then(function (locRes) {
       // write query to page so user knows what they are viewing
-      resultTextEl.textContent = locRes.search.query;
+      //resultContentEl.textContent = locRes.name;
 
       console.log(locRes);
+      console.log(locRes.name.length);
 
-      if (!locRes.results.length) {
+      if (!locRes.name.length) {
         console.log('No results found!');
         resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
       } else {
         resultContentEl.textContent = '';
-        for (var i = 0; i < locRes.results.length; i++) {
-          printResults(locRes.results[i]);
-        }
-      }
+        printTodayResults(locRes);
+     }
     })
     .catch(function (error) {
       console.error(error);
